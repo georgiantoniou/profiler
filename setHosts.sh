58dc3d438f897fb5fb5d70b7120db9bdfa6c0ef4
Georgia Antoniou <63106778+georgiantoniou@users.noreply.github.com>
2023-01-09 09:55:12 +0000

    Create setHosts.sh

---
+++ setHosts.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export ANSIBLE_HOST_KEY_CHECKING=False
+export PROFILER_HOME=/tmp
+
+if [ -z $1 ];
+  echo "wrong arguments pass number of nodes to monitor and range"
+  echo "e.g ./setHosts.sh 2 2 4"
+  exit
+else
+  start=$3
+  end=$4
+fi
+
+echo "[profiler]" > hosts
+for i in [$start..$end];
+do
+  echo "node$i" >> hosts
+done
+
